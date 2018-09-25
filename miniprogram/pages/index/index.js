3379038072813ba2665d9909b4f676c8813f5fb9
unknown <tanglianrui@TANGLIANRUI.sogou-inc.com>
2018-09-25 16:57:45 +0800

    add search function

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,8 +5,19 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
 
+  data: {
+    inputShowed: false,
+    inputVal: "",
+    tools: [
+      { id: 0, name: "ABCD2卒中评分量表", url: "TIA"},
+      { id: 1, name: "房颤患者卒中风险评估", url: "CHA2DS2-VASc" },
+      { id: 2, name: "类风湿关节炎患者病情评价", url: "DAS28" },
+      { id: 3, name: "CSF-IgG指数", url: "CSF-IgG" },
+      { id: 4, name: "Wells诊断标准(DVT)", url: "Wells-DVT" },
+      { id: 5, name: "Duke平板运动评分", url: "Duke" }
+    ],
+    search_res: []
   },
 
   /**
@@ -63,5 +74,50 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  showInput: function () {
+    this.setData({
+      inputShowed: true,
+      search_res: []
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false,
+      search_res: []
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: "",
+      search_res: []
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+
+    var inputstr = e.detail.value;
+    var all_tools = this.data.tools;
+    var match_tools = [];
+
+    console.log(inputstr);
+    for(var i = 0; i < all_tools.length; i++)
+    {
+      console.log(all_tools[i].name);
+      console.log(all_tools[i].name.indexOf(inputstr));
+      if (all_tools[i].name.indexOf(inputstr) != -1)
+      {
+        match_tools.push(all_tools[i]);
+      }
+    }
+
+    this.setData({
+      search_res: match_tools
+    })
+    console.log(match_tools);
   }
 })
